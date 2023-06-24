6d45d3051742b00b7dc59816646730d2f9e06d7f
Mustafa Ulas-MSI <mustafaulas@hotmail.com>
2023-06-25 00:27:55 +0300

    Seeders Blogs Seeds Users -> asd@asd.com

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Blog;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            BlogsSeeder::class
+        ]);
+
+        DB::table('users')->insert([
+            'name' => "asd",
+            'email' => "asd@asd.com",
+            'password' => Hash::make('asd'),
+        ]);
+
+       // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',

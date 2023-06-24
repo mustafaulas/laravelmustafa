17fdcc4a5f5ece9cd7e17fb02259f890aaf28233
Mustafa Ulas-MSI <mustafaulas@hotmail.com>
2023-06-25 01:27:46 +0300

    home Blade tasarım değişikliği

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -2,21 +2,19 @@
 @section("content")
 <div class="container" style="min-height: 600px;">
     @foreach($posts as $post)
-        <div style="padding: 10px; margin: 10px; border: 1px solid #9ca3af " class="col-12">
-            <div class="card bg-dark text-white" style="max-width: 400px;">
+        <div class="row">
+            <div style="padding: 10px; margin: 10px; border: 1px solid #e2e8f0 " class="col-sm">
                 <img src="{{ asset("Blogs/Photos/".$post->photo) }}" class="card-img" alt="{{ $post->name }}" width="400" height="250"/>
-                <div class="card-img-overlay" style="background-color: rgba(0, 0, 0, 0.3)">
-                    <h5 class="card-title">{{ $post->name }}</h5>
-                    <p class="card-text">
-                        {{ $post->summary }}
-                    </p>
-                    <p class="card-text">{{ $post->create_at }}</p>
-                </div>
             </div>
-            <div>
-                <a class="btn btn-primary" href="{{ route("show",$post->id) }}">
-                    Detay
-                </a>
+            <div style="padding: 10px; margin: 10px; border: 1px solid #e2e8f0 " class="col-8">
+                <h3>{{ $post->name }}</h3>
+                <p>{{ $post->summary }}</p>
+                <div>
+                    <p class="card-text">{{ $post->created_at->day }} / {{ $post->created_at->month }} / {{ $post->created_at->year }}</p>
+                    <a class="btn btn-primary" href="{{ route("show",$post->id) }}">
+                        Detay
+                    </a>
+                </div>
             </div>
         </div>
     @endforeach
